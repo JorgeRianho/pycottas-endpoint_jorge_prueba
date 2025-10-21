3aa14a00d5f36ee5190590e2985fd214339f2ff9
Jorge Riaño <jorgerianodelacruz@gmail.com>
2025-10-21 08:29:56 +0200

    eliminado run_endpoint innecesario

--- endpoint/run_endpoint.py
+++
@@ -1,8 +0,0 @@
-from rdflib_endpoint.__main__ import run_serve
-
-# Archivos de entrada RDF (por ejemplo, TTLs generados por Morph-KGC)
-files = ["mapping.ttl", "data.ttl"]
-
-# Inicia el endpoint en localhost:8000 con Oxigraph
-run_serve(files=files, host="localhost", port=8000, store="Oxigraph", enable_update=False)
-
